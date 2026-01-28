7476c7647246d23a02989552806f1f4f81b8989d
BEAST <imyogeshgaikwad@gmail.com>
2026-01-28 00:03:47 +0000

    Store Action And Reducers

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 import Todo from './components/todo'
 import './App.css'
+import {Provider} from "react-redux";
+import { store } from './app/store';
 
 function App() {
 
   return (
     <>
-     <Todo/>
+    <Provider store={store}>
+       <Todo/>
+    </Provider>
+    
     </>
   )
 }
--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,7 +1,15 @@
+import {useSelector} from "react-redux"
+
+
 export default function Todo(){
+    const todos =useSelector((state)=>state.todos)
+    console.log(todos);
     return(
         <div>
             <h3>Todos</h3>
+            <ul>
+                {todos.map((todo)=>(<li key={todo.id}></li>))}
+            </ul>
         </div>
     )
 }
